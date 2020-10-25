360809b7492389112bd9af463a7550af556680f5
daoinc <takuya@kirari.com>
2020-10-25 17:20:14 +0900

    ユーザー一覧画面追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
 
-user.create(:name => "山田　太郎", :sex => 1, :birthday => "2011-11-14")
-user.create(:name => "佐藤　花子", :sex => 2, :birthday => "2012-02-08")
-user.create(:name => "田中　葵", :sex => 3, :birthday => "2013-10-25")
+User.create(:name => "山田　太郎", :sex => 1, :birthday => "2011-11-14")
+User.create(:name => "佐藤　花子", :sex => 2, :birthday => "2012-02-08")
+User.create(:name => "田中　葵", :sex => 3, :birthday => "2013-10-25")
